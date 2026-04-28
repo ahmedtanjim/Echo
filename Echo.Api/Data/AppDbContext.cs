2e76596d65748a7d21652f03de1de89b5c6e5086
Tanjim Ahmed <tanjim.crazybot@gmail.com>
2026-04-29 03:00:57 +0600

    Fixed some issues

---
+++ Echo.Api/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Echo.Api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace Echo.Api.Data
+
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ChatMessage> ChatMessages { get; set; }
+    }
+}
---
+++ Echo.Client/Models/ChatMessageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Echo.Client.Models
+{
+    public class ChatMessageDto
+    {
+        public string User { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
+    }
+}
