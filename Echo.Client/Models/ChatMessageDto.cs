b16ff9593f2fe6981bac2c36f76d425acee8495f
Tanjim Ahmed <tanjim.crazybot@gmail.com>
2026-04-29 02:57:12 +0600

    Freash Commit

--- Echo.Client/Models/ChatMessageDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Echo.Client.Models
-{
-    public class ChatMessageDto
-    {
-        public string User { get; set; } = string.Empty;
-        public string Message { get; set; } = string.Empty;
-        public DateTime SentAt { get; set; } = DateTime.UtcNow;
-    }
-}
